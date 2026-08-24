83b1f9b517e60cb1c143d654dc7a04a4a3940615
anuththiga <anuththiga25jt@gmail.com>
2026-08-24 23:31:04 +0800

    added folder structer

---
+++ backend/src/models/Record.ts
@@ -0,0 +1,11 @@
+interface csvRecord {
+    id: string;
+    postId: string;
+    name: string;
+    email: string;
+    pendingData: string | null;
+    version: number;
+    createdAt: Date;
+    updatedAt: Date;
+    updatedBy: string;
+}
